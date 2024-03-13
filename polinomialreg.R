3430d367b3b92603ebe0e54001abff0f6e7c2094
SzGabor <60741863+SzGabor1@users.noreply.github.com>
2024-03-13 10:25:36 +0100

    Add files via upload

--- polinomialreg.R
+++ polinomialreg.R
@@ -1,32 +1,71 @@
 CalcA2 <- function(X,Y){
 
 
-#top
-sumx2y <- Summary(X^2,Y^1) 
-sumx3 <- Summary(X^3)
-sumx2 <- Summary(X^2) 
-sumxy <- Summary(X^1,Y^1) 
-sumx2 <- Summary(X^2) 
-sumx <- Summary(X^1) 
-sumy <- Summary(Y^1) 
-n = length(x)
+	#top
+	sumx2y <- Summary(X^2,Y^1) 
+	sumx3 <- Summary(X^3)
+	sumx2 <- Summary(X^2) 
+	sumxy <- Summary(X^1,Y^1) 
+	sumx2 <- Summary(X^2) 
+	sumx <- Summary(X^1) 
+	sumy <- Summary(Y^1) 
+	n = length(X)
 
-#bottom
-sumx4 <-Summary(X^4)
+	#bottom
+	sumx4 <-Summary(X^4)
 
 
 
-#determinant
-#upper
+	#determinants
+	#upper
 
-det_upper = sumx2y*(sumx2*n - sumx * sumx) - sumx3*(sumxy*n - sumy*sumx) + sumx2*(sumxy*sumx - sumy*sumx2)
-print(det_upper)
-det_bottom = sumx4*(sumx2*n-sumx*sumx) - sumx3*(sumx3*n - sumx2*sumx) + sumx2*(sumx3*sumx - sumx2*sumx2)
-print(det_bottom)
-#divide 2dets
-A2= det_upper/det_bottom
-print(A2)
+	det_upper = sumx2y*(sumx2*n - sumx * sumx) - sumx3*(sumxy*n - sumy*sumx) + sumx2*(sumxy*sumx - sumy*sumx2)
+	print(det_upper)
+	det_bottom = sumx4*(sumx2*n-sumx*sumx) - sumx3*(sumx3*n - sumx2*sumx) + sumx2*(sumx3*sumx - sumx2*sumx2)
+	print(det_bottom)
+	#divide 2dets
+	A2= det_upper/det_bottom
+	print(A2)
 }
+
+
+CaclA1 <- function(X,Y){
+	#top
+	sumx2y <- Summary(X^2,Y^1) 
+	sumx3 <- Summary(X^3)
+	sumx2 <- Summary(X^2) 
+	sumxy <- Summary(X^1,Y^1) 
+	sumx2 <- Summary(X^2) 
+	sumx <- Summary(X^1) 
+	sumy <- Summary(Y^1) 
+	n = length(X)
+
+	#bottom
+	sumx4 <-Summary(X^4)
+
+
+#A1 top
+
+
+#A1 bottom
+x4 <- Summary(X^4)
+x2y<-Summary(X^2,Y)
+x2<- Summary(X^2)
+x3<- Summary(X^3)
+xy <-Summary(X,Y)
+x<- Summary(X^3)
+x2<-Summary(X^2)
+y<-Summary(Y)
+
+n = length(X)
+
+	
+
+
+}
+
+
+
 
 Summary <- function(X, Y) {
   sum <- 0
@@ -43,7 +82,6 @@ Summary <- function(X, Y) {
   }
   return(sum)
 }
-
-X = rnorm(300);
-Y = rnorm(300);
+X=c(1,2,3,4)
+Y=c(15,33,61,99)
 CalcA2(X,Y);
