ba0ac9c3f130dad6e023c5d6365f29fee5cd9955
SzGabor <60741863+SzGabor1@users.noreply.github.com>
2024-03-13 10:56:05 +0100

    Add files via upload

--- polinomialreg.R
+++ polinomialreg.R
@@ -20,9 +20,9 @@ CalcA2 <- function(X,Y){
 	#upper
 
 	det_upper = sumx2y*(sumx2*n - sumx * sumx) - sumx3*(sumxy*n - sumy*sumx) + sumx2*(sumxy*sumx - sumy*sumx2)
-	print(det_upper)
+	#print(det_upper)
 	det_bottom = sumx4*(sumx2*n-sumx*sumx) - sumx3*(sumx3*n - sumx2*sumx) + sumx2*(sumx3*sumx - sumx2*sumx2)
-	print(det_bottom)
+	#print(det_bottom)
 	#divide 2dets
 	A2= det_upper/det_bottom
 	print(A2)
@@ -32,24 +32,23 @@ CalcA2 <- function(X,Y){
 CalcA1 <- function(X,Y){
  
 #A1 top-bottom
-x4 <- Summary(X^4)
-x2y<-Summary(X^2,Y)
-x2<- Summary(X^2)
-x3<- Summary(X^3)
-xy <-Summary(X,Y)
-x<- Summary(X)
-x2<-Summary(X^2)
+sumx4 <- Summary(X^4)
+sumx2y<-Summary(X^2,Y)
+sumx2<- Summary(X^2)
+sumx3<- Summary(X^3)
+sumxy <-Summary(X,Y)
+sumx<- Summary(X)
 
-y<-Summary(Y)
+sumy<-Summary(Y)
 
 n = length(X)
 
 
 
-	det_top = x4*(xy*n-y*x) - x2y *(x3*n-x2*x) + x2*(x3*y-x2*xy)
-	print(det_top)
-	det_bottom = x4*(x2*n-x*x) - x3*(x3*n-x2*x) + x2*(x3*x-x2*x2)
-	print(det_bottom)
+	det_top = sumx4*(sumxy*n-sumy*sumx) - sumx2y *(sumx3*n-sumx2*sumx) + sumx2*(sumx3*sumy-sumx2*sumxy)
+	#print(det_top)
+	det_bottom = sumx4*(sumx2*n-sumx*sumx) - sumx3*(sumx3*n-sumx2*sumx) + sumx2*(sumx3*sumx-sumx2*sumx2)
+	#print(det_bottom)
 	
 	A1=det_top/det_bottom
 	print(A1)
@@ -70,9 +69,9 @@ CalcA0 <- function(X, Y) {
   sumy <- Summary(Y)
 
   upper <- sumx4 * (sumx2 * sumy - sumx * sumxy) - sumx3 * (sumx3 * sumy - sumx2 * sumxy) + sumx2y * (sumx3 * sumx - sumx2 * sumx2)
-	print(upper)  
-bottom <- sumx4 * (sumx2 * n - sumx * sumx) - sumx3 * (sumx3 * n - sumx2 * sumx) + sumx2 * (sumx3 * sumx - sumx2 * sumx2)
-print(bottom)
+	#print(upper)  
+	bottom <- sumx4 * (sumx2 * n - sumx * sumx) - sumx3 * (sumx3 * n - sumx2 * sumx) + sumx2 * (sumx3 * sumx - sumx2 * sumx2)
+#print(bottom)
   A0 <- upper / bottom
   print(A0)
 }
@@ -94,8 +93,19 @@ Summary <- function(X, Y) {
   }
   return(sum)
 }
+
+Polreg <- function(){
 X=c(1,2,3,4)
 Y=c(15,33,61,99)
-CalcA2(X,Y);
-CalcA1(X,Y);
-CalcA0(X,Y);
+A2 = CalcA2(X,Y);
+A2 = CalcA1(X,Y);
+A2 = CalcA0(X,Y);
+
+plot(X,Y)
+curve(A2*X^2+A1*X+A0, type="1", lty = 1, col = red)
+
+}
+
+
+Polreg()
+
