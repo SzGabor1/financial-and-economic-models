bb37887a68e174e63b21ed7ab7bbe74870397126
SzGabor <60741863+SzGabor1@users.noreply.github.com>
2024-03-13 10:40:00 +0100

    Add files via upload

--- polinomialreg.R
+++ polinomialreg.R
@@ -29,25 +29,9 @@ CalcA2 <- function(X,Y){
 }
 
 
-CaclA1 <- function(X,Y){
-	#top
-	sumx2y <- Summary(X^2,Y^1) 
-	sumx3 <- Summary(X^3)
-	sumx2 <- Summary(X^2) 
-	sumxy <- Summary(X^1,Y^1) 
-	sumx2 <- Summary(X^2) 
-	sumx <- Summary(X^1) 
-	sumy <- Summary(Y^1) 
-	n = length(X)
-
-	#bottom
-	sumx4 <-Summary(X^4)
-
-
-#A1 top
-
-
-#A1 bottom
+CalcA1 <- function(X,Y){
+ 
+#A1 top-bottom
 x4 <- Summary(X^4)
 x2y<-Summary(X^2,Y)
 x2<- Summary(X^2)
@@ -55,15 +39,43 @@ x3<- Summary(X^3)
 xy <-Summary(X,Y)
 x<- Summary(X^3)
 x2<-Summary(X^2)
+
 y<-Summary(Y)
 
 n = length(X)
 
+
+
+	det_top = x4*(xy*n-y*x) - x2y *(x3*n-x2*x) + x2*(x3*y-x2*xy)
+	print(det_top)
+	det_bottom = x4*(x2*n-x*x) - x3*(x3*n-x2*x) + x2*(x3*x-x2*x2)
+	print(det_bottom)
+	
+	A1=det_top/det_bottom
+	print(A1)
 	
 
 
 }
 
+CalcA0 <- function(X, Y) {
+  n <- length(X)
+  sumx4 <- Summary(X^4)
+  sumx3 <- Summary(X^4)
+  sumx2y <- Summary(X^2, Y)
+  sumx3 <- Summary(X^3)
+  sumx2 <- Summary(X^2)
+  sumxy <- Summary(X,Y)
+  sumx <- Summary(X)
+  sumy <- Summary(Y)
+
+  upper <- sumx4 * (sumx2 * sumy - sumx * sumxy) - sumx3 * (sumx3 * sumy - sumx2 * sumxy) + sumx2y * (sumx3 * sumx - sumx2 * sumx2)
+	print(upper)  
+bottom <- sumx4 * (sumx2 * n - sumx * sumx) - sumx3 * (sumx3 * n - sumx2 * sumx) + sumx2 * (sumx3 * sumx - sumx2 * sumx2)
+print(bottom)
+  A0 <- upper / bottom
+  print(A0)
+}
 
 
 
@@ -85,3 +97,5 @@ Summary <- function(X, Y) {
 X=c(1,2,3,4)
 Y=c(15,33,61,99)
 CalcA2(X,Y);
+CalcA1(X,Y);
+CalcA0(X,Y);
